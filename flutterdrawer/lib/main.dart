ba93a9cb19d378f5d926d2792c17de41fe912e2f
ozgurustun <ozgurustundev@gmail.com>
2020-10-29 02:44:07 +0300

    drawer

---
+++ flutterdrawer/lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      theme: new ThemeData(primarySwatch: Colors.orange),
+      home: new HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        centerTitle: true,
+        title: new Text("Drawer App"),
+        elevation: defaultTargetPlatform == TargetPlatform.android ? 5.0 : 0.0,
+      ),
+      drawer: new Drawer(
+        child: new ListView(
+          children: <Widget>[
+            new ListTile(
+              title: new Text("Page 1"),
+              trailing: new Icon(Icons.arrow_right),
+            ),
+            new ListTile(
+              title: new Text("Page 2"),
+              trailing: new Icon(Icons.arrow_left),
+            ),
+            new Divider(),
+            new ListTile(
+              title: new Text("Close"),
+              trailing: new Icon(Icons.close),
+            )
+          ],
+        ),
+      ),
+      body: new Container(
+        child: new Center(
+          child: new Text("Home Page"),
+        ),
+      ),
+    );
+  }
+}
